56f11beee7acc971f436e70e773f5183fd2aa83b
Бажин Павел Аркадьевич <p.bazhin@agro.local>
2024-11-25 22:46:07 +0300

    hw11

---
+++ data.js
@@ -0,0 +1,26 @@
+let dataProductsJSON = `[
+    {
+        "image" : "1.jpg",
+        "title" : "T-SHIRT",
+        "price" : "1000",
+        "color" : "white",
+        "size" : "L",
+        "quantity" : "1"
+    },
+    {
+        "image" : "2.jpg",
+        "title" : "jeans",
+        "price" : "2000",
+        "color" : "Blue",
+        "size" : "L",
+        "quantity" : "1"
+    },
+    {
+        "image" : "3.jpg",
+        "title" : "jacket",
+        "price" : "5000",
+        "color" : "black",
+        "size" : "L",
+        "quantity" : "1"
+    }
+]`;
